867d7c99f16184d775ad41cf4ba294727db780f6
Douglas Cardoso <douglascf12@live.com>
2021-05-06 13:09:55 -0300

    video 4 finished

--- EventsApp/ViewModels/AddEventViewModel.swift
+++ EventsApp/ViewModels/AddEventViewModel.swift
@@ -5,12 +5,62 @@
 //  Created by Douglas Cardoso Ferreira on 28/04/21.
 //
 
-import Foundation
+import UIKit
 
 final class AddEventViewModel {
+    
+    let title = "Add"
+    var onUpdate: () -> Void = {}
+    
+    enum Cell {
+        case titleSubtitle(TitleSubtitleCellViewModel)
+    }
+    
+    private(set) var cells: [AddEventViewModel.Cell] = []
     var coordinator: AddEventCoordinator?
+    
+    func viewDidLoad() {
+        cells = [
+            .titleSubtitle(
+                TitleSubtitleCellViewModel(title: "Name", subtitle: "", placeholder: "Add a name", type: .text, onCellUpdate: {})
+            ),
+            .titleSubtitle(
+                TitleSubtitleCellViewModel(title: "Date", subtitle: "", placeholder: "Select a date", type: .date, onCellUpdate: { [weak self] in
+                    self?.onUpdate()
+                })
+            ),
+            .titleSubtitle(
+                TitleSubtitleCellViewModel(title: "Background", subtitle: "", placeholder: "", type: .image, onCellUpdate: { [weak self] in
+                    self?.onUpdate()
+                })
+            )
+        ]
+        onUpdate()
+    }
     
     func viewDidDisappear() {
         coordinator?.didFinishAddEvent()
     }
+    
+    func numberOfRows() -> Int {
+        return cells.count
+    }
+    
+    func cell(for indexPath: IndexPath) -> Cell {
+        return cells[indexPath.row]
+    }
+    
+    func tappedDone() {
+        print("tappedDone")
+        //extract info from cell view models and save in core data
+        //tell coordinator to dismiss
+    }
+    
+    func updateCell(indexPath: IndexPath, subtitle: String) {
+        switch cells[indexPath.row] {
+        case .titleSubtitle(let titleSubtitleCellViewModel):
+            titleSubtitleCellViewModel.update(subtitle)
+        }
+    }
+    
 }
